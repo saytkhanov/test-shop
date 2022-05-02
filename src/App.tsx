32a7f52f37117db19b3d1910b6fa1e16365ee951
saytkhanov <saythanov2014@mail.ru>
2022-05-03 00:09:01 +0300

    create project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Provider } from 'react-redux'
+import Header from './components/Header'
+import Products from './components/Products'
+import { store, persistor } from './store/store'
+
+import { PersistGate } from 'redux-persist/integration/react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Provider store={store}>
+			<PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+				<Header />
+				<Products />
+			</PersistGate>
+		</Provider>
+	)
 }
 
-export default App;
+export default App
